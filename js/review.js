c88956e35c08417f864b975fba3bff924af72f4a
yujin <ps5045@naver.com>
2023-10-24 16:49:49 +0900

    nextId fix

--- js/review.js
+++ js/review.js
@@ -1,9 +1,8 @@
 const reviewForm = document.getElementById("review-form");
 const reviews = document.getElementById("reviews-list");
 
-let nextId = localStorage.getItem("nextId")
-  ? parseInt(localStorage.getItem("nextId"))
-  : 1;
+let nextId =
+  Math.max(0, ...storedReviews.map((review) => review.id)) + 1;
 
 // Load reviews from local storage when the page loads
 const storedReviews = JSON.parse(
