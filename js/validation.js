d1840d58037a5ee2c912778dc781e60dbbd0a198
wlduq0150 <wlduq0150@gmail.com>
2023-10-24 16:50:43 +0900

    test

---
+++ js/validation.js
@@ -0,0 +1,39 @@
+const abuseURL = "https://blog.kakaocdn.net/dn/o8jTC/btrerzoVaaI/OiKT6hhdON0BCnVFY3kYQK/fword_list.txt?attach=1&knm=tfile.txt";
+
+let abuses;
+
+async function loadAbuse() {
+    const response = await fetch(abuseURL);
+    const responseText = await response.text();
+
+    abuses = responseText.split("\n");
+}
+
+function searchValidationCheck(value) {
+    // 욕설 필터링
+    for (let abuse of abuses) {
+        if (value.includes(abuse)) {
+            console.log("욕설은 제외 해야 합니다.");
+            return false;
+        }
+    }
+}
+
+function commentValidationCheck(value) {
+    // 글자수 필터링
+    const textLength = value.length;
+    if (textLength < 5 || textLength > 50) {
+        console.log("글자 수는 5~50자 입니다");
+        return false;
+    }
+
+    // 욕설 필터링
+    for (let abuse of abuses) {
+        if (value.includes(abuse)) {
+            console.log("욕설은 제외 해야 합니다.");
+            return false;
+        }
+    }
+}
+
+loadAbuse();
