6594ed505c619a04e48b176e91a7aa19dbae5dd6
Jo Park <39757235+joeunparkdev@users.noreply.github.com>
2023-10-24 15:28:13 +0900

    comments

--- js/review.js
+++ js/review.js
@@ -1,7 +1,13 @@
 const reviewForm = document.getElementById("review-form");
-
 const reviews = document.getElementById("reviews-list");
 
+// Load reviews from local storage when the page loads
+const storedReviews = JSON.parse(localStorage.getItem("reviews") || "[]");
+
+// Display the stored reviews
+for (const review of storedReviews) {
+  displayReview(review);
+}
 
 reviewForm.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -10,31 +16,40 @@ reviewForm.addEventListener("submit", function (e) {
   const reviewText = document.getElementById("review").value;
   const password = document.getElementById("password").value;
 
-  const reviewElement = document.createElement("div");
-  reviewElement.classList.add("user-review");
-  reviewElement.innerHTML = `
-    <strong>작성자:</strong> ${name}<br>
-    <strong>리뷰:</strong> ${reviewText}<br>
-    <strong>확인 비밀번호:</strong> ${password}
-    <button class="delete-button">Delete</button>
-  `;
-  reviews.appendChild(reviewElement);
+  const reviewData = {
+    name: name,
+    reviewText: reviewText,
+    password: password
+  };
+
+  // Add the review to the reviews list and save it to local storage
+  displayReview(reviewData);
+  storedReviews.push(reviewData);
+  localStorage.setItem("reviews", JSON.stringify(storedReviews));
 
   reviewForm.reset();
 });
 
-const deleteButton = reviewElement.querySelector(".delete-button");
-deleteButton.addEventListener("click", () => {
-  reviewsList.removeChild(reviewElement);
-});
+function displayReview(reviewData) {
+  const reviewElement = document.createElement("div");
+  reviewElement.classList.add("user-review");
+  reviewElement.innerHTML = `
+    <strong>작성자:</strong> ${reviewData.name}<br>
+    <strong>리뷰:</strong> ${reviewData.reviewText}<br>
+    <strong>확인 비밀번호:</strong> ${reviewData.password}
+    <button class="delete-button">Delete</button>
+  `;
 
-function displayReviews() {
-    reviews.innerHTML = '';
-  
-    const reviewElements = document.querySelectorAll('.user-review');
-    reviewElements.forEach((element) => {
-      reviews.appendChild(element);
-    });
-  }
-  
-  displayReviews();
+  const deleteButton = reviewElement.querySelector(".delete-button");
+  deleteButton.addEventListener("click", () => {
+    reviews.removeChild(reviewElement);
+    // Remove the review from the stored reviews and update local storage
+    const index = storedReviews.indexOf(reviewData);
+    if (index !== -1) {
+      storedReviews.splice(index, 1);
+      localStorage.setItem("reviews", JSON.stringify(storedReviews));
+    }
+  });
+
+  reviews.appendChild(reviewElement);
+}
