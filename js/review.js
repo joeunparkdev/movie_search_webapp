179c4b3564b36a3873a4b09743d4b473a72e2e89
yujin <ps5045@naver.com>
2023-10-25 12:31:21 +0900

    Refactoring review.js

--- js/review.js
+++ js/review.js
@@ -1,28 +1,81 @@
 const reviewForm = document.getElementById("review-form");
 const reviews = document.getElementById("reviews-list");
 
-// Load reviews from local storage when the page loads
-const storedReviews = JSON.parse(
-  localStorage.getItem("reviews") || "[]"
-);
+let storedReviews = loadReviewsFromLocalStorage();
 
-let nextId =
-  storedReviews.length > 0
-    ? Math.max(...storedReviews.map((review) => review.id)) + 1
-    : 1;
+const movieId = getMovieIdFromUrl();
+let nextId = parseInt(localStorage.getItem("nextId")) || 1;
 
-const urlParams = new URLSearchParams(window.location.search);
-const movieId = urlParams.get("id");
+filterAndDisplayReviews(storedReviews, movieId);
 
-const filteredReviews = storedReviews.filter(
-  (review) => review.movieId === movieId
-);
+reviewForm.addEventListener("submit", handleReviewSubmission);
 
-// Display the stored reviews
-for (const review of filteredReviews) {
-  displayReview(review);
+async function handleReviewSubmission(e) {
+  e.preventDefault();
+
+  const nameInputValue = getInputValue("name");
+  const reviewInputValue = getInputValue("review");
+  const passwordInputValue = getInputValue("password");
+
+  // 유효성 검사
+  if (!commentValidationCheck(reviewInputValue)) {
+    resetForm(reviewForm);
+    return;
+  }
+
+  const hashedPassword = await hashPassword(passwordInputValue);
+
+  nextId++;
+
+  const newReviewData = {
+    id: nextId,
+    movieId: movieId,
+    name: nameInputValue,
+    reviewText: reviewInputValue,
+    password: hashedPassword,
+  };
+
+  addNewReview(newReviewData);
+  resetForm(reviewForm);
 }
 
+// 로컬 스토리지 가져오기
+function loadReviewsFromLocalStorage() {
+  return JSON.parse(localStorage.getItem("reviews")) || [];
+}
+
+// 입력값 가져오기
+function getInputValue(inputElementId) {
+  return document.getElementById(inputElementId).value;
+}
+
+// url에서 영화 id 추출
+function getMovieIdFromUrl() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const movieId = urlParams.get("id");
+  return movieId;
+}
+
+// 새 리뷰 생성
+function addNewReview(reviewData) {
+  displayReview(reviewData);
+  storedReviews.push(reviewData);
+  localStorage.setItem("reviews", JSON.stringify(storedReviews));
+  localStorage.setItem("nextId", nextId.toString());
+}
+
+// 리뷰 영화별 필터링
+function filterAndDisplayReviews(reviews, movieId) {
+  const filteredReviews = reviews.filter(
+    (review) => review.movieId === movieId
+  );
+
+  for (const review of filteredReviews) {
+    displayReview(review);
+  }
+}
+
+// 비밀번호 hash 로직
 async function hashPassword(password) {
   try {
     const encoder = new TextEncoder();
@@ -42,102 +95,107 @@ async function hashPassword(password) {
   }
 }
 
-reviewForm.addEventListener("submit", async function (e) {
-  e.preventDefault();
-
-  const name = document.getElementById("name").value;
-  const reviewText = document.getElementById("review").value;
-  const password = document.getElementById("password").value;
-
-  if (!commentValidationCheck(reviewText)) {
-    reviewForm.reset();
-    return;
-  }
-  // Hash the user's password before storing it
-  const hashedPassword = await hashPassword(password);
-
-  const reviewData = {
-    id: nextId++,
-    movieId: movieId,
-    name: name,
-    reviewText: reviewText,
-    password: hashedPassword,
-  };
-
-  // Add the review to the reviews list and save it to local storage
-  displayReview(reviewData);
-  storedReviews.push(reviewData);
-  localStorage.setItem("reviews", JSON.stringify(storedReviews));
-  localStorage.setItem("nextId", nextId.toString());
-
-  reviewForm.reset();
-});
-
+// 리뷰 보여주기
 function displayReview(reviewData) {
   const reviewElement = document.createElement("div");
   reviewElement.classList.add("user-review");
   reviewElement.innerHTML = `
     <strong>작성자:</strong> ${reviewData.name}<br>
     <strong>리뷰:</strong> <span class="review-text">${reviewData.reviewText}</span><br>
-    <strong>확인 비밀번호:</strong> ${reviewData.password}
     <button class="delete-button">Delete</button>
     <button class="edit-button">Edit</button>
   `;
 
+  // 삭제 버튼 정의
   const deleteButton = reviewElement.querySelector(".delete-button");
 
-  deleteButton.addEventListener("click", async () => {
-    const enteredPassword = prompt("비밀번호를 입력하세요.");
-    const enteredPasswordHash = await hashPassword(enteredPassword);
+  // 삭제 버튼 이벤트
+  deleteButton.addEventListener("click", () =>
+    handleDeleteClick(reviewData, reviewElement)
+  );
 
-    if (enteredPasswordHash === reviewData.password) {
-      if (reviews.contains(reviewElement)) {
-        reviews.removeChild(reviewElement);
-      }
-      removeReviewFromStorage(reviewData.id);
-    } else {
-      alert("비밀번호가 틀렸습니다.");
-    }
-  });
-
+  // 수정 버튼 정의
   const editButton = reviewElement.querySelector(".edit-button");
 
-  editButton.addEventListener("click", async () => {
-    const enteredPassword = prompt("비밀번호를 입력하세요.");
-    const enteredPasswordHash = await hashPassword(enteredPassword);
-    if (enteredPasswordHash === reviewData.password) {
-      let newReview = prompt("새로운 리뷰 내용을 입력하세요");
-      updateStoredReview(newReview, reviewData.id);
-
-      let newTextElememt =
-        reviewElement.querySelector(".review-text");
-      newTextElememt.textContent = newReview;
-
-      window.location.reload();
-    } else {
-      alert("비밀번호가 틀렸습니다.");
-    }
-  });
+  // 수정 버튼 이벤트
+  editButton.addEventListener("click", () =>
+    handleEditClick(reviewData, reviewElement)
+  );
 
   reviews.appendChild(reviewElement);
+}
 
-  function removeReviewFromStorage(id) {
-    let index = storedReviews.findIndex((review) => {
-      return id === review.id;
-    });
-    if (index !== -1) {
-      storedReviews.splice(index, 1);
-      localStorage.setItem("reviews", JSON.stringify(storedReviews));
-    }
+// 리뷰 삭제
+function removeReviewFromStorage(id) {
+  let index = storedReviews.findIndex((review) => {
+    return id === review.id;
+  });
+  if (index !== -1) {
+    storedReviews.splice(index, 1);
+    localStorage.setItem("reviews", JSON.stringify(storedReviews));
+  }
+}
+
+// 리뷰 수정
+function updateStoredReview(newText, id) {
+  let index = storedReviews.findIndex((review) => {
+    return id === review.id;
+  });
+  if (index !== -1) {
+    storedReviews[index].reviewText = newText;
+    localStorage.setItem("reviews", JSON.stringify(storedReviews));
+  }
+}
+
+// 삭제 버튼 핸들러
+async function handleDeleteClick(reviewData, reviewElement) {
+  const enteredPassword = prompt("비밀번호를 입력하세요.");
+  const enteredPasswordHash = await hashPassword(enteredPassword);
+
+  // 취소 버튼을 누른 경우 처리
+  if (enteredPassword === null) {
+    return;
   }
 
-  function updateStoredReview(newText, id) {
-    let index = storedReviews.findIndex((review) => {
-      return id === review.id;
-    });
-    if (index !== -1) {
-      storedReviews[index].reviewText = newText;
-      localStorage.setItem("reviews", JSON.stringify(storedReviews));
+  if (enteredPasswordHash === reviewData.password) {
+    if (reviews.contains(reviewElement)) {
+      reviews.removeChild(reviewElement);
     }
+    removeReviewFromStorage(reviewData.id);
+  } else {
+    alert("비밀번호가 틀렸습니다.");
   }
+}
+
+// 수정 버튼 핸들러
+async function handleEditClick(reviewData, reviewElement) {
+  const enteredPassword = prompt("비밀번호를 입력하세요.");
+  const enteredPasswordHash = await hashPassword(enteredPassword);
+
+  // 취소 버튼을 누른 경우 처리
+  if (enteredPassword === null) {
+    return;
+  }
+
+  if (enteredPasswordHash === reviewData.password) {
+    let newReviewText = prompt("새로운 리뷰 내용을 입력하세요");
+
+    // 유효성 검사
+    if (!commentValidationCheck(newReviewText)) {
+      alert("유효하지 않은 리뷰입니다.");
+      return;
+    }
+
+    updateStoredReview(newReviewText, reviewData.id);
+
+    let newTextElememt = reviewElement.querySelector(".review-text");
+    newTextElememt.textContent = newReviewText;
+  } else {
+    alert("비밀번호가 틀렸습니다.");
+  }
+}
+
+// 초기화
+function resetForm(formElement) {
+  formElement.reset();
 }
