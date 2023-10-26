3563f5f5c6f124a0013cda2c412a2cd8ed60d240
yujin <ps5045@naver.com>
2023-10-26 11:26:40 +0900

    realtime db set

--- js/firebase/firebaseConfig.js
+++ js/firebase/firebaseConfig.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAfEU1tg8luekB86eddJrf2NxR8bw4nkZ4",
@@ -14,6 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app);
+const db = getDatabase(app);
 
 export { auth, db };
