fceabf10f7b323cb1beac3b96e60adc64b2c6d11
yujin <ps5045@naver.com>
2023-10-27 00:29:01 +0900

    전체 삭제, 즐찾 사용자 수, 즐찾 페이지 즐겨찾기 버튼 구현 및 자잘한 수정

--- js/favorite.js
+++ js/favorite.js
@@ -1,97 +1,140 @@
-import { db } from "./firebase/firebaseConfig.js";
-import { ref, onValue } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
+import {
+  getAuth,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
+import { auth } from "./firebase/firebaseConfig.js";
+import { removeAllFavoriteMovies } from "./firebase/databaseService.js";
+import { getFavoriteMovies as getFavoriteMovieIds } from "./firebase/databaseService.js";
+import {
+  createFavoriteIcon,
+  getFavoriteCount,
+} from "./firebase/databaseService.js";
 
 function createFavoriteCard(movie) {
-    const favoriteList = document.querySelector(".favorite_list");
-    const col = document.createElement("div");
-    col.classList.add("col");
+  const favoriteList = document.querySelector(".favorite_list");
+  const col = document.createElement("div");
+  col.classList.add("col");
 
-    const card = document.createElement("div");
-    card.classList.add("card", "card_");
-    card.dataset.id = movie.id;
+  const card = document.createElement("div");
+  card.classList.add("card", "card_");
+  card.dataset.id = movie.id;
 
-    const img = document.createElement("img");
-    img.classList.add("card-img-top");
-    img.src = posterURL + movie.poster_path;
-    card.appendChild(img);
+  const img = document.createElement("img");
+  img.classList.add("card-img-top");
+  img.src = posterURL + movie.poster_path;
+  card.appendChild(img);
 
-    const button = document.createElement("button");
-    button.type = "button";
-    button.classList.add("detail_button", "btn", "btn-danger");
-    button.textContent = "상세보기";
-    card.appendChild(button);
+  const button = document.createElement("button");
+  button.type = "button";
+  button.classList.add("detail_button", "btn", "btn-danger");
+  button.textContent = "상세보기";
+  card.appendChild(button);
 
-    const span = document.createElement("span");
-    span.classList.add("movie_title");
-    span.textContent = movie.title;
-    card.appendChild(span);
+  const span = document.createElement("span");
+  span.classList.add("movie_title");
+  span.textContent = movie.title;
+  card.appendChild(span);
 
-    const p = document.createElement("p");
-    p.classList.add("movie_overview");
-    p.textContent = movie.overview;
-    card.appendChild(p);
+  const favoriteCountSpan = document.createElement("span");
+  favoriteCountSpan.classList.add("favorite_count");
+  card.appendChild(favoriteCountSpan);
 
-    const average = document.createElement("span");
-    average.classList.add("movie_average");
-    average.textContent = movie.vote_average;
-    card.appendChild(average);
+  getFavoriteCount(movie.id).then((count) => {
+    favoriteCountSpan.textContent = `즐겨찾기한 사용자 수: ${count}명`;
+  });
 
-    // movie.poster_path가 null인 경우에 대비하여 기본 이미지 URL을 사용
-    img.src = movie.poster_path ? posterURL + movie.poster_path : "기본 이미지 URL 또는 다른 이미지 URL";
+  const p = document.createElement("p");
+  p.classList.add("movie_overview");
+  p.textContent = movie.overview;
+  card.appendChild(p);
 
-    initEventCard(card);
+  const average = document.createElement("span");
+  average.classList.add("movie_average");
+  average.textContent = movie.vote_average;
+  card.appendChild(average);
 
-    col.appendChild(card);
-    favoriteList.appendChild(col);
+  // movie.poster_path가 null인 경우에 대비하여 기본 이미지 URL을 사용
+  img.src = movie.poster_path
+    ? posterURL + movie.poster_path
+    : "기본 이미지 URL 또는 다른 이미지 URL";
+
+  const isLoggedIn = auth.currentUser ? true : false;
+  let favoriteIcon = card.querySelector(".favorite");
+
+  if (favoriteIcon) {
+    card.removeChild(favoriteIcon);
+  }
+  if (isLoggedIn) {
+    favoriteIcon = createFavoriteIcon(movie.id);
+    card.appendChild(favoriteIcon);
+  }
+
+  initEventCard(card);
+
+  col.appendChild(card);
+  favoriteList.appendChild(col);
 }
 
 function deleteFavoriteCard() {
-    let list = document.querySelectorAll(".favorite_list .col");
-    list.forEach((col) => {
-      col.remove();
-    });
-  }
+  let list = document.querySelectorAll(".favorite_list .col");
+  list.forEach((col) => {
+    col.remove();
+  });
+}
 
 async function getMovieForId(movieId) {
-    try {
-        const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?language=ko-KR&api_key=${API_KEY}`);
-        if (!response.ok) {
-            console.log("API 요청 에러");
-        }
-        const movieData = await response.json();
-        return movieData;
-    } catch (e) {
-        console.log("API 요청 에러");
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}?language=ko-KR&api_key=${API_KEY}`
+    );
+    if (!response.ok) {
+      console.log("API 요청 에러");
     }
+    const movieData = await response.json();
+    return movieData;
+  } catch (e) {
+    console.log("API 요청 에러");
+  }
 
-    return null;
+  return null;
 }
 
+// 수정했어요.
+// 기존에 databaseService 에 있던거 가져다씀
 async function getFavoriteMovies() {
-    deleteFavoriteCard();
+  deleteFavoriteCard();
 
-    const auth = await getAuth();
-    if (!auth.currentUser) {
-        alert("로그인이 필요합니다!");
-        return;
-    }
+  const auth = await getAuth();
+  if (!auth.currentUser) {
+    alert("로그인이 필요합니다!");
+    return;
+  }
 
-    const user = auth.currentUser.uid;
+  const user = auth.currentUser.uid;
 
-    const favoriteRef = ref(db, `users/${user}/favorites`);
+  const favoriteMovieIds = await getFavoriteMovieIds(user);
 
-    onValue(favoriteRef, async (snapshot) => {
-        const favoriteData = snapshot.val();
-
-        for (let id of favoriteData) {
-            const movie = await getMovieForId(id);
-            console.log(movie);
-            createFavoriteCard(movie);
-        }
-    });
+  for (let id of favoriteMovieIds) {
+    const movie = await getMovieForId(id);
+    console.log(movie);
+    createFavoriteCard(movie);
+  }
 }
+
+const deleteAllFavoriteMovies =
+  document.getElementById("delAllFavBtn");
+
+deleteAllFavoriteMovies.addEventListener("click", async () => {
+  const user = auth.currentUser;
+  if (user) {
+    await removeAllFavoriteMovies(user.uid);
+    alert("모든 즐겨찾기 항목이 삭제되었습니다.");
+    window.location.reload();
+  } else {
+    console.log("사용자가 로그인하지 않았습니다.");
+  }
+});
 
 onAuthStateChanged(getAuth(), (user) => {
-    setTimeout(getFavoriteMovies, 1000);
+  getFavoriteMovies();
 });
