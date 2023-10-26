0fb48dc2c0019723876d9f1e6ec99f894c6e241c
wlduq0150 <wlduq0150@gmail.com>
2023-10-26 18:31:11 +0900

    토큰 재발급

--- js/secret.js
+++ js/secret.js
@@ -1,5 +1,5 @@
 const API_KEY = "d8035bf70702e0aadedc78c740ad3979";
-const ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkODAzNWJmNzA3MDJlMGFhZGVkYzc4Yzc0MGFkMzk3OSIsInN1YiI6IjY0OTA2OGEwNTU5ZDIyMDBjNTc3Y2NmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zgJ4vvK5mlvpnFjAYrjMCN8ebE4bHd8hj6MhvvEngww";
+const ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMTQ2NDVmNjIzMWNhZGUzMGUyM2ZmNDNjYTRhNDU5MiIsInN1YiI6IjY0OTA2OGEwNTU5ZDIyMDBjNTc3Y2NmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dajWXO2OnvuGTy25Z3H9-b0Z49TQPoG5Vegf1mIgOuY";
 
 const searchURL = "https://api.themoviedb.org/3/search/movie?language=ko-KR&query=";
 const popularURL = "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=";
